7b5791b66340ac08f723f4c21ed4f988668dc85b
ralex <ralex.rdz@gmail.com>
2017-05-02 17:37:56 -0500

    extended intent model

--- src/models/intent.js
+++ src/models/intent.js
@@ -1,16 +1,27 @@
 const Mongoose = require('mongoose')
 
+// refator in new model - Also used in project model
+const placeSchema = new Mongoose.Schema({
+  address: { type: String },
+  latitude: { type: Number },
+  longitude: { type: Number }
+})
+
 const intentSchema = new Mongoose.Schema({
   _id: { type: String },
   type: { type: String },
   projects: [{ type: String, ref: 'Project' }],
   title: { type: String },
   description: { type: String },
+  logo: { type: String },
+  admins: [{ type: String }],
+  creator: { type: String },
   status: { type: String, default: 'active' },
   direction: { type: String }, // offer || request
   collaborationType: { type: String },
   condition: { type: String },
-  expiryDate: { type: String }
+  expiryDate: { type: String },
+  locations: [placeSchema]
 })
 
 const Intent = Mongoose.model('Intent', intentSchema, 'intents')
